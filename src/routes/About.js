ac1df2c1454142bb1a5cdc018cf9c37990610ad3
Dj-Jes <95280143+Dj-Jes@users.noreply.github.com>
2023-04-10 10:28:46 +0200

    last changes

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
+import './About.css';
+
 const About = () => {
     return (
         <div className="aboutContainer">
-            <h1>About this app</h1>
             <p>
-                This app is build with React framework<br/>
-                All styling are made in css<br/>
-
+                <br />
             </p>
+
+            <div className="textContainer">
+                <h1>About this app</h1>
+                <p>
+                    This app is built with React framework<br />
+                    All styling is made in CSS<br />
+                </p>
+            </div>
         </div>
+
     );
 };
 
